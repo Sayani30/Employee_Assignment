8a3f107decd93b2a995c0a44ec0668ed217bb210
Sayani30 <60376290+Sayani30@users.noreply.github.com>
2020-01-30 21:32:57 +0530

    Update EmployeeMain.java

--- src/com/cognizant/service/EmployeeMain.java
+++ src/com/cognizant/service/EmployeeMain.java
@@ -14,7 +14,7 @@ public class EmployeeMain {
 	 * Driver method
 	 */
 	public static void main(String[] args) throws IOException {
-		File file = new File("abc.csv");
+		File file = new File("Mock_Data.csv");
 		Collection<Employee> employeeList = Utility.getEmployeeList(file);
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter offset : ");
